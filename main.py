8813c541189971ead0fd9228f68158f9cb99bf13
Furnac <shmarglefoil@gmail.com>
2017-05-18 21:25:57 -0400

    stuffs going down yo

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ displayFrame = Frame(root, width=400, height=400)
 optionsFrame = Frame(root, width=400, height=100)
 
 typeLabel = Label(optionsFrame, text='Type:')
-typeTextbox = Text(optionsFrame, width=20, height=1)
+typeOption = OptionMenu(optionsFrame, StringVar(), 'Mandlebrot', 'Julia Set')
 zoomLabel = Label(optionsFrame, text='Zoom:')
 zoomTextbox = Text(optionsFrame, width=20, height=1)
 
-typeLabel.grid(row=0, column=0)
-typeTextbox.grid(row=0, column=1)
+typeLabel.grid(row=0, column=0, sticky='w')
+typeOption.grid(row=0, column=1, sticky='w')
 zoomLabel.grid(row=0, column=2)
 zoomTextbox.grid(row=0, column=3)
 
